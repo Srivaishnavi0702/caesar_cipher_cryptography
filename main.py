249c1cbd650dcae70e2cd9a64c93ec3a052a7442
Srivaishnavi0702 <anbusrivaishnavi@gmail.com>
2026-02-17 22:42:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# TODO-1: Import and print the logo from art.py when the program starts.
+# Importing logo
 from art import logo
-
 print(logo)
 
+# Source letters
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# TODO-2: What happens if the user enters a number/symbol/space?
-
-
+# Encode & Decode function
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
     if encode_or_decode == "decode":
@@ -21,15 +19,14 @@ def caesar(original_text, shift_amount, encode_or_decode):
             output_text += letter
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-
-# TODO-3: Can you figure out a way to restart the cipher program?
+# User-input function
 def user_input():
-
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
     caesar(original_text = text, shift_amount = shift, encode_or_decode = direction)
 
+# Continuation
 user_input()
 while True:
     next_phase = input("Type 'yes' if you want to go again. Otherwise, type 'no':\n").lower()
